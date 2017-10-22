55fb66f598169694afcaee2ffb0bfaab83f4d022
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-10-22 12:46:45 -0700

    Removed code in script.js files.

--- begining-javascript-5ed/data-types-and-variables/ex-1/script.js
+++ begining-javascript-5ed/data-types-and-variables/ex-1/script.js
@@ -1,10 +1,1 @@
-// function degToFahrenheit(degCent) {
-//     degFahren = ((9 / 5) * degCent) + 32;
-//     return degFahren;
-// }
-
-
-function degToFahrenheit(degCent) {
-    degFahren = ((9 / 5) * degCent) + 32;
-    return degCent + " deg. Celsius, converted is " + degFahren + " deg. Fahrenheit";
-}
+// Your Code Here
